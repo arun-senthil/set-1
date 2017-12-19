85be4cf91beaa15cac65eb3ec834ec0001af9194
arun-senthil <34536732+arun-senthil@users.noreply.github.com>
2017-12-19 18:13:30 +0530

    sum_of_n.c

---
+++ som_of_n.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int i,n=0,num;
+	printf("enter the numbers : ");
+	scanf("%d",&num);
+	for(i=0;i<=num;i++)
+	{
+		n=n+1;
+		
+	}
+	printf("\nthe sum %d numbers is : %d",num,n);
+}
