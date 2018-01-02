de685e60f108073fee1a55e874dfa16f8ab77c4b
arunachala-manikandan <34536732+arunachala-manikandan@users.noreply.github.com>
2018-01-01 22:41:38 -0800

    mutiples

---
+++ multiples.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//code
+	int n,i,multiples;
+	cout<<"enter the number : ";
+	cin>>n;
+	for(i=1;i<=5;i++)
+	{
+	    multiples=n*i;
+	 cout<<multiples<<" ";   
+	}
+	return 0;
+}
